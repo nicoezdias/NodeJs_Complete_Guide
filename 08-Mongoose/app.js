e109b983f269a247f2bf15256a2592e5f6f4a1ad
nicoezdias <nicolasezdias@gmail.com>
2023-02-09 16:17:35 -0300

    Add mongoose structure

---
+++ 08-Mongoose/app.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const dotenv = requiere('dotenv');
+const express = require('express');
+const bodyParser = require('body-parser');
+const errorController = require('./controllers/error');
+const mongoConnect = require('./db/mongodbConfig').mongoConnect;
+const User = require('./models/user');
+const adminRoutes = require('./routes/admin.routes');
+const shopRoutes = require('./routes/shop.routes');
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+	User.findById('63e43470d2c8987044dd92f1')
+		.then((user) => {
+			req.user = new User(user.name, user.email, user.cart, user._id);
+			next();
+		})
+		.catch((err) => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+	app.listen(PORT);
+});
