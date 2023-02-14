b25e69282dd647730c9e1cfa857bf20fb2a2ee66
nicoezdias <nicolasezdias@gmail.com>
2023-02-14 14:13:55 -0300

    Add Sessions&Cookies

--- 09-Sessions&Cookies/app.js
+++ 09-Sessions&Cookies/app.js
@@ -3,25 +3,40 @@ const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 const adminRoutes = require('./routes/admin.routes');
 const shopRoutes = require('./routes/shop.routes');
-
+const authRoutes = require('./routes/auth.routes');
 const app = express();
-mongoose.set('strictQuery', false);
-dotenv.config();
 const PORT = process.env.PORT || 4000;
+const store = new MongoDBStore({
+	uri: process.env.DB_URL,
+	collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	session({
+		secret: 'my secret',
+		resave: false,
+		saveUninitialized: false,
+		store: store,
+	})
+);
 
 app.use((req, res, next) => {
-	User.findById('63e5829718731e218ccf65c7')
+	if (!req.session.user) {
+		return next();
+	}
+	User.findById(req.session.user._id)
 		.then((user) => {
 			req.user = user;
 			next();
@@ -31,6 +46,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
