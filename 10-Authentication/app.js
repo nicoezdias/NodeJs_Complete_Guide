166343c0ced593e91617fcf4f4f9fcf55a93be32
nicoezdias <nicolasezdias@gmail.com>
2023-02-14 14:16:12 -0300

    Add structure

---
+++ 10-Authentication/app.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const dotenv = require('dotenv');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+const adminRoutes = require('./routes/admin.routes');
+const shopRoutes = require('./routes/shop.routes');
+const authRoutes = require('./routes/auth.routes');
+const app = express();
+const PORT = process.env.PORT || 4000;
+const store = new MongoDBStore({
+	uri: process.env.DB_URL,
+	collection: 'sessions',
+});
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	session({
+		secret: 'my secret',
+		resave: false,
+		saveUninitialized: false,
+		store: store,
+	})
+);
+
+app.use((req, res, next) => {
+	if (!req.session.user) {
+		return next();
+	}
+	User.findById(req.session.user._id)
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+	.connect(process.env.DB_URL)
+	.then((result) => {
+		User.findOne().then((user) => {
+			if (!user) {
+				const user = new User({
+					name: 'Max',
+					email: 'max@test.com',
+					cart: {
+						items: [],
+					},
+				});
+				user.save();
+			}
+		});
+		app.listen(PORT);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
