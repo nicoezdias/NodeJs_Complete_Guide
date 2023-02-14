557f5b3ee35c7b254b796a19273421b47daa2774
nicoezdias <nicolasezdias@gmail.com>
2023-02-14 15:47:47 -0300

    Add structure

---
+++ 12-AdvancedAuthentication/app.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const dotenv = require('dotenv');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+const adminRoutes = require('./routes/admin.routes');
+const shopRoutes = require('./routes/shop.routes');
+const authRoutes = require('./routes/auth.routes');
+const app = express();
+const PORT = process.env.PORT || 4000;
+const store = new MongoDBStore({
+	uri: process.env.DB_URL,
+	collection: 'sessions',
+});
+const csrfProtection = csrf();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	session({
+		secret: 'my secret',
+		resave: false,
+		saveUninitialized: false,
+		store: store,
+	})
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+	if (!req.session.user) {
+		return next();
+	}
+	User.findById(req.session.user._id)
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => console.log(err));
+});
+
+app.use((req, res, next) => {
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+	.connect(process.env.DB_URL)
+	.then((result) => {
+		app.listen(PORT);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
