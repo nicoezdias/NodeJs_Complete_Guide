cb834fbf49e9de07c25e87201b71e92a6959c3bb
nicoezdias <nicolasezdias@gmail.com>
2023-02-16 17:10:28 -0300

    Add Error Handling

--- 14-Payments/app.js
+++ 14-Payments/app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -10,59 +10,55 @@ const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
-
-const MONGODB_URI =
-  'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop';
-
+const adminRoutes = require('./routes/admin.routes');
+const shopRoutes = require('./routes/shop.routes');
+const authRoutes = require('./routes/auth.routes');
 const app = express();
+const PORT = process.env.PORT || 4000;
 const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: 'sessions'
+	uri: process.env.DB_URL,
+	collection: 'sessions',
 });
 const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({
-    secret: 'my secret',
-    resave: false,
-    saveUninitialized: false,
-    store: store
-  })
+	session({
+		secret: 'my secret',
+		resave: false,
+		saveUninitialized: false,
+		store: store,
+	})
 );
 app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
 });
 
 app.use((req, res, next) => {
-  // throw new Error('Sync Dummy');
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then(user => {
-      if (!user) {
-        return next();
-      }
-      req.user = user;
-      next();
-    })
-    .catch(err => {
-      next(new Error(err));
-    });
+	// throw new Error('Sync Dummy');
+	if (!req.session.user) {
+		return next();
+	}
+	User.findById(req.session.user._id)
+		.then((user) => {
+			if (!user) {
+				return next();
+			}
+			req.user = user;
+			next();
+		})
+		.catch((err) => {
+			next(new Error(err));
+		});
 });
 
 app.use('/admin', adminRoutes);
@@ -74,20 +70,18 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  // res.status(error.httpStatusCode).render(...);
-  // res.redirect('/500');
-  res.status(500).render('500', {
-    pageTitle: 'Error!',
-    path: '/500',
-    isAuthenticated: req.session.isLoggedIn
-  });
+	// res.status(error.httpStatusCode).render(...);
+	// res.redirect('/500');
+	res.status(500).render('500', {
+		pageTitle: 'Error!',
+		path: '/500',
+		isAuthenticated: req.session.isLoggedIn,
+	});
 });
 
 mongoose
-  .connect(MONGODB_URI)
-  .then(result => {
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+	.connect(process.env.DB_URL)
+	.then((result) => {})
+	.catch((err) => {
+		console.log(err);
+	});
